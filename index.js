492fb582b187179ff727e60a15c7c4e9141bce01
srinanoo <srinanoo@gmail.com>
2025-01-04 15:32:41 +0530

    update redis read

--- index.js
+++ index.js
@@ -7,41 +7,40 @@ const REDIS_PORT = 6379;
 
 const app = express();
 const redisClient = redis.createClient(REDIS_PORT);
-redisClient.on('error', err => console.error(err));
+redisClient.on('error', err => console.log(err));
 redisClient.connect().then(() => console.log('Redis Connected!'));
 
 function redisCache(req, res, next) {
-    try {
-        const { name } = req.params;
-    
-        redisClient.get(name, (err, data) => {
-            if(err) throw err;
-            console.log(data);
-            if(data !== null) {
-                res.status(200).send(data);
-            } else {
-                next();
-            }
-        })
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
+    const { name } = req.params;
+    console.log(name, "redis");
+
+    redisClient.get(name).then(data => {
+        if(data !== null) {
+            console.log(data, "redis data");
+            res.status(200).send(data);
+        } else {
+            next();
+        }
+    });
 }
 
-function getRole(req, res, next) {
+function getRole(req, res) {
     try {
         const { name } = req.params;
+        console.log(name, "server");
         let role;
         if(name) {
-            fs.readFile("./data/trainees.json", "utf8", (err, data) => {
+            fs.readFile("./data/trainees.json", "utf8", async (err, data) => {
                 if(err) throw err;
                 data = JSON.parse(data);
                 if(data.length > 0) {
                     for(let i = 0; i < data.length; i++) {
                         if(data[i].name === name) {
                             role = data[i].role;
-                            // redisClient.set(name, role);
-                            redisClient.setEx(name, 3600, role);
+                            console.log(role);
+                            redisClient.set(name, role).then(res => console.log(res, "redis set"));
+                            // redisClient.setEx(name, 3600, role).then(res => console.log(res, "redis set"));
+                            // redisClient.get(name).then(res => console.log(res, "redis get"));
                             break;
                         }
                     }
@@ -53,6 +52,6 @@ function getRole(req, res, next) {
         res.status(500).send(err.message);
     }
 }
-app.get('/api/trainees/:name', getRole);
+app.get('/api/trainees/:name', redisCache, getRole);
 
 app.listen(PORT, () => console.log(`Server is listening at ${PORT}`));
